545a36f2bcbc145cf80b45e3b24c4f8066ba13a8
Bob van der Linden <bobvanderlinden@gmail.com>
2018-05-13 01:11:05 +0200

    travis: stage all files inside nixpkgs-ruby

--- .travis/push.sh
+++ .travis/push.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 cd nixpkgs-ruby
-git add versions
+git add --all
 git commit --message "Travis build: $TRAVIS_BUILD_NUMBER"
 git push https://${GITHUB_TOKEN}@github.com/bobvanderlinden/nixpkgs-ruby master
