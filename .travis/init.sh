244c7140f546a69af5b0d79d4ec86ef625666cac
Bob van der Linden <bobvanderlinden@gmail.com>
2018-05-13 01:59:30 +0200

    travis: always refresh all versions

--- .travis/init.sh
+++ .travis/init.sh
@@ -3,3 +3,4 @@ git config --global user.email "travis@travis-ci.org"
 git config --global user.name "Travis CI"
 git clone https://${GITHUB_TOKEN}@github.com/bobvanderlinden/nixpkgs-ruby nixpkgs-ruby
 cp -f ruby_default.nix nixpkgs-ruby/default.nix
+rm -rf nixpkgs-ruby/versions
