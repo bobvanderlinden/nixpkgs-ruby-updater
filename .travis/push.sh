8f518e0a183a0c9ce5271f0597dc66e3982b9e6b
Bob van der Linden <bobvanderlinden@gmail.com>
2018-07-21 00:52:53 +0200

    another travis update

--- .travis/push.sh
+++ .travis/push.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 cd nixpkgs-ruby
-git commit --all --message "Travis build: $TRAVIS_BUILD_NUMBER" .
+git add .
+git commit --message "Travis build: $TRAVIS_BUILD_NUMBER"
 git push https://${GITHUB_TOKEN}@github.com/bobvanderlinden/nixpkgs-ruby master
