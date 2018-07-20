14ba656148dbbc15a0756baeb297362a205881a8
Bob van der Linden <bobvanderlinden@gmail.com>
2018-07-21 00:50:26 +0200

    commit all files in travis

--- .travis/push.sh
+++ .travis/push.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 cd nixpkgs-ruby
-git commit --all --message "Travis build: $TRAVIS_BUILD_NUMBER"
+git commit --all --message "Travis build: $TRAVIS_BUILD_NUMBER" .
 git push https://${GITHUB_TOKEN}@github.com/bobvanderlinden/nixpkgs-ruby master
